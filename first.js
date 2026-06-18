bfcd83a69be629ad887dfa0a404ee979532226c0
anindomahmood <anindomahmood1@gmail.com>
2026-06-18 10:56:22 +0600

    Initial commit

---
+++ first.js
@@ -0,0 +1,18 @@
+alert("welcome to JavaScript");
+console.log("Hello World");
+
+name="John Doe";
+console.log("My name is " + name);
+
+age=24;
+console.log("My age is " + age);
+
+x=null;
+console.log("The value of x is " + x);
+
+y=undefined;
+console.log("The value of y is " + y);
+
+booleanValue=true;
+console.log("I am Follow the trend - " + booleanValue);
+
