8e9fa2b671565bb3a5dc79e4240f0c57c16e37b8
anindomahmood <anindomahmood1@gmail.com>
2026-06-19 13:51:41 +0600

    update

--- first.js
+++ first.js
@@ -16,3 +16,33 @@ console.log("The value of y is " + y);
 booleanValue=true;
 console.log("I am Follow the trend - " + booleanValue);
 
+a=prompt("Please enter your name: ");
+console.log("The name you entered is " + a);
+
+let m=15;
+if(m>10){
+    console.log("m is greater than 10");
+}
+
+class Student {
+    constructor(name, age, id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+}
+
+let student1=new Student("Anindo", 25, 55004);
+console.log("Student Name: " + student1.name);
+console.log("Student Age: " + student1.age);
+console.log("Student ID: " + student1.id);
+
+let student2 =
+{
+    name:prompt("Enter student name: "),
+    age:parseInt(prompt("Enter student age: ")),
+    id:parseInt(prompt("Enter student ID: "))
+}
+console.log("Student Name: " + student2.name);
+console.log("Student Age: " + student2.age);
+console.log("Student ID: " + student2.id);
